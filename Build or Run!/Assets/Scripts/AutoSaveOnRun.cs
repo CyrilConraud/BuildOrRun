f725547b8b1def2760871de01cc154b5179052d2
Cyril <cyril.conraud@gmail.com>
2019-01-11 21:52:08 +0100

    Folders sorting

---
+++ Build or Run!/Assets/Scripts/AutoSaveOnRun.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_EDITOR
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AutoSaveOnRun : MonoBehaviour
+{
+
+    static AutoSaveOnRun()
+    {
+        EditorApplication.playmodeStateChanged = () =>
+        {
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+            {
+                //Debug.Log("Auto-Saving scene before entering Play mode: " + EditorApplication.currentScene);
+
+                EditorApplication.SaveScene();
+                AssetDatabase.SaveAssets();
+            }
+        };
+    }
+}
+
+#endif
---
+++ Build or Run!/Assets/Scripts/CrashMe.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class CrashMe : MonoBehaviour {
+
+    [DllImport("kernel32.dll")]
+    static extern void RaiseException(uint dwExceptionCode, uint dwExceptionFlags, uint n, IntPtr lp);
+
+	// Use this for initialization
+	void Start () {
+        RaiseException(13, 0, 0, new IntPtr(1));
+	}
+}
